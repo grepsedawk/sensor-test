a3efad6b4c5788c3b0d0ae1f1b4e0a36ee33bb24
Alex Piechowski <alex@piechowski.org>
2017-06-01 20:33:14 -0500

    Move map_float to be private

--- libraries/Sensor/Sensor.h
+++ libraries/Sensor/Sensor.h
@@ -11,13 +11,13 @@ class Sensor {
 public:
     Sensor(int pin, double min_voltage, double max_voltage, double min_value, double max_value);
     float value();
-    float map_float(float x, float in_min, float in_max, float out_min, float out_max);
 private:
     int pin;
     double min_voltage;
     double max_voltage;
     double min_value;
     double max_value;
+    float map_float(float x, float in_min, float in_max, float out_min, float out_max);
 };
 
 
