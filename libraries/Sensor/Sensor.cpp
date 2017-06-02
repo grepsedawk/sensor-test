3ec15ea09f1674f7ee3061eadfd84b9565a38d01
Alex Piechowski <alex@piechowski.org>
2017-06-01 20:34:11 -0500

    Codestyle

--- libraries/Sensor/Sensor.cpp
+++ libraries/Sensor/Sensor.cpp
@@ -18,7 +18,6 @@ float Sensor::value() {
   return map(value, min_voltage, max_voltage, min_value, max_value);
 }
 
-float Sensor::map_float(float x, float in_min, float in_max, float out_min, float out_max)
-{
+float Sensor::map_float(float x, float in_min, float in_max, float out_min, float out_max) {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
--- libraries/Sensor/Sensor.h
+++ libraries/Sensor/Sensor.h
@@ -10,13 +10,16 @@
 class Sensor {
 public:
     Sensor(int pin, double min_voltage, double max_voltage, double min_value, double max_value);
+
     float value();
+
 private:
     int pin;
     double min_voltage;
     double max_voltage;
     double min_value;
     double max_value;
+
     float map_float(float x, float in_min, float in_max, float out_min, float out_max);
 };
 
